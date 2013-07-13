e9411cb2b2f8fa5cd1dbe71c48a2b8b28332f87d
Leigh Anne Glasson <laglasson@yahoo.com>
2013-07-13 01:35:40 +0000

    fix

--- web.js
+++ web.js
@@ -1,16 +1,21 @@
+#!/usr/bin/env node
 var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buffer = new Buffer(fs.readFile('~/bitstarter/index.html', function (err, data) {
-  if (err) throw err;
+var buffer = new Buffer(128);
+fs.readFile('~/bitstarter/index.html', function (err, data) {
+  app.get('/', function(request, response) {
+     response.send(buffer.ToString(data));
+  });
+console.log("after readFile");
 });
 
-app.get('/', function(request, response) {
-  response.send(buffer.ToString(data));
-});
+// app.get('/', function(request, response) {
+  // response.send(buffer.ToString(data));
+// });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
