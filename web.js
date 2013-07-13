c73d1379c612ef629cf21410c766e055de47b37d
Leigh Anne Glasson <laglasson@yahoo.com>
2013-07-13 00:41:45 +0000

    Update to read message from file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer(fs.readFile('~/bitstarter/index.html', function (err, data) {
+  if (err) throw err;
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.ToString(data));
 });
 
 var port = process.env.PORT || 5000;
