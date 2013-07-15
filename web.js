601f220b49d6b33cdb353cd4356dc0f1d2b352e0
Leigh Anne Glasson <laglasson@yahoo.com>
2013-07-15 22:04:15 +0000

    fix file stuff

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
-#!/usr/bin/env node
 var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buffer = new Buffer(128);
-buffer=fs.readFile('~/bitstarter/index.html'); 
-//     response.send(buffer.ToString(data));
 
-console.log("Buffer:"+buffer);
+app.get('/', function(request, response) {
 
+var buff = new Buffer(128);
+buff = fs.readFileSync('index.html');
+var str1 = buff.toString();
+// console.log(str1 + "\n");
+response.send(str1);
 
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
